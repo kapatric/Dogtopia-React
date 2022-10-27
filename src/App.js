4c2a63ffdc21e5dbc7b088b510b81e9d0629c88b
kapatric <112514185+kapatric@users.noreply.github.com>
2022-10-27 11:41:41 -0500

    can pull 1 dog

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react"
+import axios from "axios";
 import './App.css';
 
-function App() {
+export default function Breed() {
+  const [data, setData] = useState();
+  const url = {
+    method: 'GET',
+    url: 'https://api.thedogapi.com/v1/breeds',
+    headers: {
+      'X-RapidAPI-Key': 'live_nWWQoLtH76j9sn8zN1QQPM56cYfwtERfwW06wUR6xPSxsDFXIaIZ3fk73kVzoRAW',
+      Accept: "application/json",
+    }
+  };
+
+  useEffect(() => {
+    fetchBreed()
+  }, []);
+  
+
+  let fetchBreed = () => {
+    fetch(url.url, url.headers)
+    .then((res) => res.json())
+    .then((res) => console.log(res[0].name))
+    .then((res) => setData(res))
+  }
+
+
+
+
+console.log(data)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav className="Navbar">
+        <h1  className="nav-branding">Art BASEL</h1>
+          <div className="Breed">{data}</div>
+            <div className="hamburger">
+              <span className="bar"></span>
+              <span className="bar"></span>
+              <span className="bar"></span>
+            </div>
+          </nav>
       </header>
     </div>
   );
 }
 
-export default App;
+
